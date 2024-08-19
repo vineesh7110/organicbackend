5e4c71e6bc09a2713ace9c5e3a6c4b31fffbc9bc
vineesh <vineesh27011999@gmail.com>
2024-08-19 15:17:51 +0530

    third commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const cors = require('cors')
 
 require('dotenv').config()
 app.use(cors({
-  origin:["http://localhost:5173"],
+  origin:["https://organicfrontend.vercel.app/"],
   methods:["GET","POST","PATCH","DELETE"],
   credentials:true
 }))

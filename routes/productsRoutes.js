b13b8c730437ef7e7782ae20bb2f142c4d130028
vineesh7110 <135145507+vineesh7110@users.noreply.github.com>
2025-10-13 14:35:28 +0530

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,6 +1,6 @@
 
 const express = require('express')
-const router = express()
+const router = express.Router(); 
 const Product = require('../models/products')
 
 router.post('/', async (req, res) => {

7259cf492f2fab70a20a368d797774c5a430b512
vineesh7110 <135145507+vineesh7110@users.noreply.github.com>
2025-10-13 16:27:01 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 
 
-{/* const express = require('express')
-const router = express()
+ const express = require('express');
+
+const router = express.Router();
 const User = require('../models/user')
 
 router.post('/', async (req, res) => {
@@ -54,4 +55,4 @@ router.post('/', async (req, res) => {
     }
   
   })
-module.exports = router   */}
+module.exports = router;

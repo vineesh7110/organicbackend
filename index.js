4c03ed59b2e3a00ef03d82f9a609d60316e67070
vineesh7110 <135145507+vineesh7110@users.noreply.github.com>
2025-10-16 17:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken');
-const router = express.Router()
+
 const User = require('./models/user') 
 const Product = require('./models/products')
 const Contact = require('./models/contact')
